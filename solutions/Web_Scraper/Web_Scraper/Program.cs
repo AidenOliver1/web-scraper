7deb2be595cfb3bca203b2d2f6e7f9ec164b9e03
AidenOliver1 <94108662+AidenOliver1@users.noreply.github.com>
2023-05-24 07:01:29 +0800

    Configured the core logic

---
+++ solutions/Web_Scraper/Web_Scraper/Program.cs
@@ -0,0 +1,34 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Net.Http;
+
+namespace Web_Scraper
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            // Send get request to weather.com
+            string url = "https://weather.com/en-AU/weather/today/l/131837017d4f362c0baabb987cc709163d0107e40ec35b69add9390718373e65";
+            var httpClient = new HttpClient();
+            var html = httpClient.GetStringAsync(url).Result;
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+
+            // Get the Temperature
+            var temperatureElement = htmlDocument.DocumentNode.SelectSingleNode("//span[@class='CurrentConditions--tempValue--MHmYY']");
+            var temperature = temperatureElement.InnerText.Trim();
+            Console.WriteLine(temperature);
+
+            // Get the conditions
+            var conditionElement = htmlDocument.DocumentNode.SelectSingleNode("//div[@class='CurrentConditions--phraseValue--mZC_p']");
+            var condition = conditionElement.InnerText.Trim();
+            Console.WriteLine(condition);
+
+            // Get the location
+            var locationElement = htmlDocument.DocumentNode.SelectSingleNode("//h1[@class='CurrentConditions--location--1YWj_']");
+            var location = locationElement.InnerText.Trim();
+            Console.WriteLine(location);
+        }
+    }
+}
